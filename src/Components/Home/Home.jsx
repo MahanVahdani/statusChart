7aff7c7996e76a355b95fab62aafd0c8ad439dc1
mahanvahdani <mahanvahdani76@gmail.com>
2022-12-01 11:57:07 +0330

    destructure data

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,12 +4,25 @@ import { data } from "../../Data/data";
 const Home = () => {
   return (
     <div className="flex max-w-full py-10 px-5">
-      {data.map((data, i) => (
-        <div
-          key={i}
-          className="w-1.5 h-8 mx-[2px] rounded-md bg-green-700"
-        ></div>
-      ))}
+      {data.map(
+        (
+          { time, withOutProblem, qualityLoss, ProblemUpdating, outOfService },
+          i
+        ) => (
+          <div
+            key={i}
+            className={`w-1.5 h-8 mx-[2px] rounded-md  ${
+              withOutProblem
+                ? "bg-green-700"
+                : qualityLoss
+                ? "bg-yellow-500"
+                : ProblemUpdating
+                ? "bg-blue-600"
+                : "bg-red-600"
+            }`}
+          ></div>
+        )
+      )}
     </div>
   );
 };
