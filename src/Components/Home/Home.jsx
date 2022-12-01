41df157da6827848d94e01de681f700f4d306be6
mahanvahdani <mahanvahdani76@gmail.com>
2022-12-01 14:47:05 +0330

    packageUpdates

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,28 +1,11 @@
 import React from "react";
+import Chart from "../Chart/Chart";
 import { data } from "../../Data/data";
 
 const Home = () => {
   return (
-    <div className="flex max-w-full py-10 px-5">
-      {data.map(
-        (
-          { time, withOutProblem, qualityLoss, ProblemUpdating, outOfService },
-          i
-        ) => (
-          <div
-            key={i}
-            className={`w-1.5 h-8 mx-[2px] rounded-md  ${
-              withOutProblem
-                ? "bg-green-700"
-                : qualityLoss
-                ? "bg-yellow-500"
-                : ProblemUpdating
-                ? "bg-blue-600"
-                : "bg-red-600"
-            }`}
-          ></div>
-        )
-      )}
+    <div className="bg-gray-700 flex justify-center py-52 px-10">
+      <Chart data={data} />
     </div>
   );
 };
