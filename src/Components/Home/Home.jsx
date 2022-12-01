5414107d30f3b8a7c1c7a9315f60128300c01c57
mahanvahdani <mahanvahdani76@gmail.com>
2022-12-01 11:49:02 +0330

    base implementation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { data } from "../../Data/data";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex max-w-full py-10 px-5">
+      {data.map((data, i) => (
+        <div
+          key={i}
+          className="w-1.5 h-8 mx-[2px] rounded-md bg-green-700"
+        ></div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
