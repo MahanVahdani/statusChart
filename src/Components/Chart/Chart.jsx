e9b9c2d95b1a1f33595120404fea9c049f017018
mahanvahdani <mahanvahdani76@gmail.com>
2022-12-01 13:29:25 +0330

    make size and color of bars dynamic

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Tooltip } from "@material-tailwind/react";
 
-const Chart = ({ width = "10px", height = "20px", data }) => {
+const Chart = ({
+  data,
+  width = 1.5,
+  height = 8,
+  firstColor = "#3bd671",
+  secondColor = "#f29030",
+  thirdColor = "#2f7f55",
+  fourthColor = "#df484a",
+}) => {
   const handleStatusMessage = (status) => {
     return status === 1
       ? "بدون اختلال"
@@ -17,24 +25,26 @@ const Chart = ({ width = "10px", height = "20px", data }) => {
       {data.map(({ date = "0/0/0", upTime = 100, status = 1 }, i) => (
         <Tooltip
           key={i}
+          className={`bg-[rgba(0,0,0,0.5)] text-white rounded-md px-2 py-0.5 text-xs`}
           content={
-            <div>
-              <p>{date}</p>
-              <p>upTime 90%</p>
-              <p>{handleStatusMessage(status)}</p>
+            <div className="flex flex-col justify-center items-center">
+              <p className="py-1">{"تاریخ: " + date}</p>
+              <p className="py-1">{"upTime: " + upTime + "%"}</p>
+              <p className="py-1">{handleStatusMessage(status)}</p>
             </div>
           }
-          className="bg-[rgba()] text-white rounded-md px-2 py-0.5 text-xs"
         >
           <div
-            className={`w-2 h-10 mx-[2px] rounded-md ${
+            className={`mx-[2px] rounded-md hover:brightness-50 transition-all duration-100 
+            ${"w-" + width}  ${"h-" + height}
+            ${
               status === 1
-                ? "bg-[#3bd671]"
+                ? "bg-[" + firstColor + "]"
                 : status === 2
-                ? "bg-[#f29030]"
+                ? "bg-[" + secondColor + "]"
                 : status === 3
-                ? "bg-[#2f7f55]"
-                : "bg-[#df484a]"
+                ? "bg-[" + thirdColor + "]"
+                : "bg-[" + fourthColor + "]"
             }`}
           />
         </Tooltip>
